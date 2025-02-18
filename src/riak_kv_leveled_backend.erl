0873690c5f573f8dd306755a025b9e984d221f59
Martin Sumner <martin.sumner@adaptip.co.uk>
2025-02-18 20:17:51 +0000

    Don't restart leveled backend on drop/1

--- src/riak_kv_leveled_backend.erl
+++ src/riak_kv_leveled_backend.erl
@@ -78,7 +78,7 @@
     % accepting HEAD/GET/FOLD requests).  There is no neat way of doing this
     % so we will back everything off.
 
--record(state, {bookie :: pid(),
+-record(state, {bookie :: pid()|dropped,
                 reference :: reference(),
                 partition :: integer(),
                 db_path :: string(),
@@ -577,9 +577,9 @@ fold_heads(FoldHeadsFun, Acc, Opts, #state{bookie=Bookie}) ->
 
 %% @doc Delete all objects from this leveled backend
 -spec drop(state()) -> {ok, state()} | {error, term(), state()}.
-drop(#state{bookie=Bookie, partition=Partition, config=Config}=_State) ->
+drop(#state{bookie=Bookie}=State) ->
     ok = leveled_bookie:book_destroy(Bookie),
-    start(Partition, Config).
+    {ok, State#state{bookie = dropped}}.
 
 %% @doc Returns true if this leveled backend contains any
 %% non-tombstone values; otherwise returns false.
